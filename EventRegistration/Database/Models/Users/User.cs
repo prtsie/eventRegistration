62cf61d83bbd56a223c26fc837fe8fc836ab0ca3
prtsie <yonhsup@mail.ru>
2024-09-27 19:11:11 +0300

    Атрибуты с ограничениями для User.cs

--- EventRegistration/Database/Models/Users/User.cs
+++ EventRegistration/Database/Models/Users/User.cs
@@ -1,4 +1,6 @@
-﻿namespace EventRegistration.Database.Models.Users
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventRegistration.Database.Models.Users
 {
     /// <summary> Модель пользователя </summary>
     public class User
@@ -7,9 +9,13 @@
         public Guid Id { get; } = Guid.NewGuid();
 
         /// <summary> Логин для идентификации пользователя </summary>
+        [Required]
+        [MaxLength(Constraints.MaxLoginLength)]
         public required string Login { get; set; }
 
         /// <summary> Пароль для аутентификации пользователя </summary>
+        [Required]
+        [MaxLength(Constraints.MaxPasswordLength)]
         public required string Password { get; set; }
 
         /// <summary> Роль пользователя, определяющая его привилегии </summary>
