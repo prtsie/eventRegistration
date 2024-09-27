f30b029e9a86342b2d8fd5b1fd1a37a648585274
prtsie <yonhsup@mail.ru>
2024-09-27 18:44:23 +0300

    Добавил модель пользователя в User.cs и роли в Role.cs

---
+++ EventRegistration/DBModels/Users/Role.cs
@@ -0,0 +1,12 @@
+﻿namespace EventRegistration.DBModels.Users
+{
+    /// <summary> Роли пользователей </summary>
+    public enum Role
+    {
+        /// <summary> Участник </summary>
+        Member,
+
+        /// <summary> Организатор </summary>
+        Organizer
+    }
+}
---
+++ EventRegistration/DBModels/Users/User.cs
@@ -0,0 +1,18 @@
+﻿namespace EventRegistration.DBModels.Users
+{
+    /// <summary> Модель пользователя </summary>
+    public class User
+    {
+        /// <summary> Уникальный дентификатор пользователя </summary>
+        public Guid Id { get; } = Guid.NewGuid();
+
+        /// <summary> Логин для идентификации пользователя </summary>
+        public required string Login { get; set; }
+
+        /// <summary> Пароль для аутентификации пользователя </summary>
+        public required string Password { get; set; }
+
+        /// <summary> Роль пользователя, определяющая его привилегии </summary>
+        public required Role Role { get; set; }
+    }
+}
