901d76af855a6cd2fe9583dcde3425b1c5565727
prtsie <yonhsup@mail.ru>
2024-09-27 19:10:12 +0300

    Константы-ограничения на длину полей в файле Constraints.cs

---
+++ EventRegistration/Database/Models/Constraints.cs
@@ -0,0 +1,13 @@
+﻿namespace EventRegistration.Database.Models
+{
+    /// <summary> Статический класс с ограничениями на длину полей </summary>
+    public static class Constraints
+    {
+        // Для модели юзера
+        public const int MaxLoginLength = 50;
+        public const int MaxPasswordLength = 50;
+
+        // Для модели мероприятия
+        public const int MaxEventNameLength = 200;
+    }
+}
