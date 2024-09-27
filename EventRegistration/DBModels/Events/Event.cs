60d1965b071b2414ddc909f62013185ead6ac0f0
prtsie <yonhsup@mail.ru>
2024-09-27 18:45:17 +0300

    Добавил модель мероприятия в Event.cs

---
+++ EventRegistration/DBModels/Events/Event.cs
@@ -0,0 +1,21 @@
+﻿using EventRegistration.DBModels.Users;
+
+namespace EventRegistration.DBModels.Events
+{
+    /// <summary> Модель мероприятия </summary>
+    public class Event
+    {
+        /// <summary> Уникальный идентификатор события </summary>
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        /// <summary> Название мероприятия </summary>
+        public required string Name { get; set; }
+
+        /// <summary> Дата и время проведения </summary>
+        public required DateTime Date { get; set; }
+
+        /// <summary> Организатор </summary>
+        /// <remarks> Навигационное свойство </remarks>
+        public required User Host { get; set; }
+    }
+}
