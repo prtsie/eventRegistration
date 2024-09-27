01f71e1c463c4ad6996f2fc485424329fccf7da3
prtsie <yonhsup@mail.ru>
2024-09-27 18:45:29 +0300

    Модель регистрации в Registration.cs

---
+++ EventRegistration/DBModels/Events/Registration.cs
@@ -0,0 +1,19 @@
+﻿using EventRegistration.DBModels.Users;
+
+namespace EventRegistration.DBModels.Events
+{
+    /// <summary> Модель записи на мероприятие </summary>
+    public class Registration
+    {
+        /// <summary> Уникальный идентификатор </summary>
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        /// <summary> Записавшийся пользователь </summary>
+        /// <remarks> Навигационное свойство </remarks>
+        public required User User { get; set; }
+
+        /// <summary> Мероприятие </summary>
+        /// <remarks> Навигационное свойство </remarks>
+        public required Event Event { get; set; }
+    }
+}
