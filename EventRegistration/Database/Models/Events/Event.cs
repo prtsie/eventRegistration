7d597cbfe26e87f977f72aaee237a7e88802c141
prtsie <yonhsup@mail.ru>
2024-09-27 19:10:32 +0300

    Атрибуты с ограничениями для Event.cs

--- EventRegistration/Database/Models/Events/Event.cs
+++ EventRegistration/Database/Models/Events/Event.cs
@@ -1,4 +1,5 @@
-﻿using EventRegistration.Database.Models.Users;
+﻿using System.ComponentModel.DataAnnotations;
+using EventRegistration.Database.Models.Users;
 
 namespace EventRegistration.Database.Models.Events
 {
@@ -6,16 +7,21 @@ namespace EventRegistration.Database.Models.Events
     public class Event
     {
         /// <summary> Уникальный идентификатор события </summary>
+        [Required]
         public Guid Id { get; set; } = Guid.NewGuid();
 
         /// <summary> Название мероприятия </summary>
+        [Required]
+        [MaxLength(Constraints.MaxEventNameLength)]
         public required string Name { get; set; }
 
         /// <summary> Дата и время проведения </summary>
+        [Required]
         public required DateTime Date { get; set; }
 
         /// <summary> Организатор </summary>
         /// <remarks> Навигационное свойство </remarks>
+        [Required]
         public required User Host { get; set; }
     }
 }
