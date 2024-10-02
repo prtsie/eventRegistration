1a3814118aedd9539459a05132b5e5807661f692
prtsie <yonhsup@mail.ru>
2024-10-02 10:51:39 +0300

    Сервис, возвращающий текущую дату и время

---
+++ EventRegistration/Services/DateTimeProvider/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace EventRegistration.Services.DateTimeProvider
+{
+    /// <summary> Сервис, возвращающий текущее системное время </summary>
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTimeOffset Now() => DateTimeOffset.Now;
+    }
+}
---
+++ EventRegistration/Services/DateTimeProvider/IDateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace EventRegistration.Services.DateTimeProvider
+{
+    /// <summary> Интерфейс для сервиса, возвращающего текущее время </summary>
+    public interface IDateTimeProvider
+    {
+        DateTimeOffset Now();
+    }
+}
