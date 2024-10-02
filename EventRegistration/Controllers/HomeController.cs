1e3a8af65a8281db8beeebab5841b8323fcfd0bb
prtsie <yonhsup@mail.ru>
2024-10-02 10:52:53 +0300

    Новые странички и actions в контроллере

--- EventRegistration/Controllers/HomeController.cs
+++ EventRegistration/Controllers/HomeController.cs
@@ -1,23 +1,56 @@
+using EventRegistration.Database;
+using EventRegistration.Database.Models.Events;
+using EventRegistration.Database.Models.Users;
+using EventRegistration.Requests;
+using EventRegistration.Services.DateTimeProvider;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EventRegistration.Controllers;
 
-public class HomeController : Controller
+public class HomeController(IDbContext context, IDateTimeProvider dateTimeProvider) : Controller
 {
-
-    public IActionResult Index()
+    [HttpGet]
+    public IActionResult Authentication()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+    [HttpPost]
+    public IActionResult Authentication(AuthenticationRequest request)
+    {
+        var user = context.GetEntities<User>().SingleOrDefault(u => u.Login == request.Login && u.Password == request.Password);
+
+        if (user is null)
+        {
+            return View(request);
+        }
+
+        return user.Role switch
+        {
+            Role.Member => RedirectToAction(nameof(Events)),
+            Role.Organizer => RedirectToAction(nameof(HostEvents)),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    [HttpGet]
+    public IActionResult HostEvents()
+    {
+        //TODO сервис для получения идентификатора пользователя
+        //TODO дописать action
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Event(Guid id)
     {
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpGet]
+    public IActionResult Events()
     {
-        return View();
+        var events = context.GetEntities<Event>().Where(e => e.Date < dateTimeProvider.Now()).ToList();
+        return View(events);
     }
 }
---
+++ EventRegistration/Requests/AuthenticationRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EventRegistration.Database.Models;
+
+namespace EventRegistration.Requests
+{
+    public class AuthenticationRequest
+    {
+        /// <summary> Логин для идентификации пользователя </summary>
+        [Required]
+        [MaxLength(Constraints.MaxLoginLength)]
+        public required string Login { get; set; }
+
+        /// <summary> Пароль для аутентификации пользователя </summary>
+        [Required]
+        [MaxLength(Constraints.MaxPasswordLength)]
+        public required string Password { get; set; }
+    }
+}
