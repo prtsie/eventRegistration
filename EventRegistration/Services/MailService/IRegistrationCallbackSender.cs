40a68aebcff728a67979f1ddef3a173e4e16e107
prtsie <yonhsup@mail.ru>
2024-10-03 19:13:58 +0300

    Отправка письма

---
+++ EventRegistration/Services/MailService/IRegistrationCallbackSender.cs
@@ -0,0 +1,9 @@
+﻿using EventRegistration.Database.Models.Events;
+
+namespace EventRegistration.Services.MailService
+{
+    public interface IRegistrationCallbackSender
+    {
+        Task SendCallbackAsync(Registration registration, CancellationToken cancellationToken);
+    }
+}
---
+++ EventRegistration/Services/MailService/MailOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace EventRegistration.Services.MailService
+{
+    public class MailOptions
+    {
+        public required string OrganizationName { get; set; }
+
+        public required string OrganizationMail { get; set; }
+
+        public required  int Port { get; set; }
+
+        public required  string SmtpServer { get; set; }
+
+        public required  string SmtpServerLogin { get; set; }
+
+        public required  string SmtpServerPassword { get; set; }
+
+        public bool UseSsl { get; set; }
+    }
+}
